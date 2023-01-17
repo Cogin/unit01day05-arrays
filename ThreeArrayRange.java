043662553d0e3ca633bccbbda414cc06338e155f
Cogin <cevinmontgomery05@gmail.com>
2023-01-17 12:36:47 -0600

    TreeArray

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,18 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {5, 1, 3};
+        int min = a[0];
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
+                    min = a[i];
+                    System.out.println("min: " + min);
+            }
+            else if (a[i] > max) {
+                    max = a[i];
+                    System.out.println("max: " + max);
+            }
+        }
+        System.out.println(max - min);
     }
 }
