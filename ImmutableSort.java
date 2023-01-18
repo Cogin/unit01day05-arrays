96f5a42976ce7460f6912057478816311e072704
Cogin <cevinmontgomery05@gmail.com>
2023-01-18 11:24:24 -0600

    Finished | ImmutableSort + MutableSort

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,26 @@
 public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {4, 3, 2};
+        int[] b = a; // b is a reference to a
+        // Sort the array without modifying a
+        //bubble sort i think
+        int temp = a[0];
         
+        for (int i = 1; i < a.length; i++) {
+            if (b[i] < b[i-1]) {
+                temp = b[i];
+                b[i] = b[i-1];
+                b[i-1] = temp;
+            }
+        }
+        //again for number of vars -1
+        for (int i = 1; i < a.length; i++) {
+            if (b[i] < b[i-1]) {
+                temp = b[i];
+                b[i] = b[i-1];
+                b[i-1] = temp;
+            }
+        }
+        System.out.println(b[0] + " " + b[1] + " " + b[2]);
     }
 }
--- MutableSort.java
+++ MutableSort.java
@@ -1,5 +1,23 @@
 public class MutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {4, 3, 2};
+        int temp = a[0];
+        
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i-1]) {
+                temp = a[i];
+                a[i] = a[i-1];
+                a[i-1] = temp;
+            }
+        }
+        //again for numaer of vars -1
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i-1]) {
+                temp = a[i];
+                a[i] = a[i-1];
+                a[i-1] = temp;
+            }
+        }
+        System.out.println(a[0] + " " + a[1] + " " + a[2]);
     }
 }
